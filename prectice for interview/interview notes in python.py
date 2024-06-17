24113ab4e27f35e7ef0dc4825ca670149ce5c74e
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-06-17 17:21:32 +0530

    Update interview notes in python.py

--- prectice for interview/interview notes in python.py
+++ prectice for interview/interview notes in python.py
@@ -1,4 +1,12 @@
+# 1. Basic Syntax and Data Types
+# Variables and Data Types: Integers, floats, strings, booleans.
+# Comments: Use # for single-line comments and triple quotes """ """ for multi-line comments.
+# Indentation: Python uses indentation to define the scope of loops, functions, and classes.
 
+x = 10        # Integer
+y = 3.14      # Float
+name = "Alice" # String
+is_active = True  # Boolean
 
 # 2. Control Flow
 # Conditional Statements: if, elif, else
